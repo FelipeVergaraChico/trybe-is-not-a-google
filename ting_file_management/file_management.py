c69ee61b06e66f5af0a5b8c0a0cb7ab9463f20a3
Felipe Vergara <felipe.vergara.chico@gmail.com>
2024-02-14 15:49:34 -0300

    req 1 e 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if '.txt' not in path_file:
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file, 'r') as file:
+            c = file.read()
+            res = c.split('\n')
+            file.close()
+            return res
+    except FileNotFoundError:
+        sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
